ca1f11f445174754954bfdfc9682eb0af8bcaead
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2022-02-17 16:04:05 +0300

    base game is done except diagonal win condition

---
+++ lib/connect_four.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+
+require_relative 'color'
+
+class Game
+  attr_accessor :board, :game_over
+
+  def initialize
+    @board = initialize_board(6, 0)
+    @column = nil
+    @row_size = 6
+    @column_size = 7
+  end
+
+  def play_game(player1 = nil, player2 = nil)
+    player1 = Player.new if player1.nil?
+    player2 = Player.new(2) if player2.nil?
+
+    play_turn(player1, player2)
+  end
+
+  def play_turn(player1, player2)
+    turn = 0
+    loop do
+      print_board
+
+      if turn.zero?
+        print "#{'Player 1'.red}'s turn: "
+        @column = gets
+        @column = @column.to_i
+        
+        if valid?(@board, @column)
+          row = next_available_row(@board, @column)
+          drop_disc(@board, row, @column, player1)
+
+          if decide_winner(@board, player1)
+            print_board
+            puts "\nPLAYER 1 WINS!".red
+            return
+          end
+        end
+
+      else
+        print "#{'Player 2'.yellow}'s turn: "
+        @column = gets.to_i
+
+        if valid?(@board, @column)
+          row = next_available_row(@board, @column)
+          drop_disc(@board, row, @column, player2)
+
+          if decide_winner(@board, player1)
+            print_board
+            puts "\nPLAYER 2 WINS!".red
+            return
+          end
+        end
+      end
+
+      turn += 1
+      turn = turn % 2
+    end
+  end
+
+  def decide_winner(board, player)
+    result = false
+    # horizontal
+    (@column_size - 3).times do |c|
+      @row_size.times do |r|
+        if board[r][c] == player.disc && board[r][c + 1] == player.disc && board[r][c + 2] == player.disc && board[r][c + 3] == player.disc
+          result = true
+        end
+      end
+    end
+
+    # vertical
+    @column_size.times do |c|
+      (@row_size - 3).times do |r|
+        if board[r][c] == player.disc && board[r + 1][c] == player.disc && board[r + 2][c] == player.disc && board[r + 3][c] == player.disc
+          result = true
+        end
+      end
+    end
+
+    # positive diagonal
+
+
+    result
+  end
+
+  def drop_disc(board, row, column, player)
+    board[row][column] = player.disc
+  end
+
+  def valid?(board, column)
+    (board[5][column]).zero?
+  end
+
+  def next_available_row(board, column)
+    @row_size.times do |i|
+      return i if (board[i][column]).zero?
+    end
+  end
+
+  def print_board
+    @board.reverse.each { |row| p row }
+  end
+
+  private
+
+  def initialize_board(n, val)
+    board = Array.new(n)
+    n.times do |row_index|
+      board[row_index] = Array.new(n)
+      7.times do |column_index|
+        board[row_index][column_index] = val
+      end
+    end
+    board
+  end
+end
+
+class Player
+  attr_accessor :disc
+
+  def initialize(disc = 1)
+    @disc = disc
+  end
+end
+
+# game = Game.new
+# game.play_game
+# p game.board[2][3]
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require_relative '../lib/connect_four'
+
+describe Game do
+  subject(:game) { described_class.new }
+  let(:board) { game.board }
+
+  describe '#play_turn' do
+    subject(:game_turn) { described_class.new }
+    let(:player1) { Player.new }
+    let(:player2) { Player.new(2) }
+
+    context 'when 4 discs are together horizontally' do
+      before do
+        allow(game_turn).to receive(:gets).and_return(0, 6, 1, 5, 2, 4, 3)
+        allow(game_turn).to receive(:print_board)
+        allow(game_turn).to receive(:print)
+      end
+
+      it 'displays a message and stops loop' do
+        message = "\nPLAYER 1 WINS!".red
+        expect(game_turn).to receive(:puts).with(message)
+        game_turn.play_turn(player1, player2)
+      end
+    end
+
+    context 'when 4 discs are together vertically' do
+      before do
+        allow(game_turn).to receive(:gets).and_return(0, 1, 0, 1, 0, 1, 0)
+        allow(game_turn).to receive(:print_board)
+        allow(game_turn).to receive(:print)
+      end
+
+      it 'displays a message and stops loop' do
+        message = "\nPLAYER 1 WINS!".red
+        expect(game_turn).to receive(:puts).with(message)
+        game_turn.play_turn(player1, player2)
+      end
+    end
+
+    context 'when 4 discs are together diagonally' do
+      before do
+        allow(game_turn).to receive(:gets).and_return(0)
+        allow(game_turn).to receive(:print_board)
+        allow(game_turn).to receive(:print)
+      end
+
+      xit 'displays a message and stops loop' do
+        message = "\nPLAYER 1 WINS!".red
+        expect(game_turn).to receive(:puts).with(message)
+        game_turn.play_turn(player1, player2)
+      end
+    end
+  end
+
+  describe '#drop_disc' do
+    context 'when dropping a disc' do
+      let(:player) { Player.new }
+
+      it 'drops the disc to an available row in the specified column' do
+        game.drop_disc(board, 2, 3, player)
+        expect(board[2][3]).to eq(player.disc)
+      end
+    end
+  end
+
+  describe '#valid?' do
+    context 'when making selection' do
+      it 'returns true if the top column is empty' do
+        expect(game.valid?(board, 4)).to eq(true)
+      end
+
+      it 'returns false if the top column is not empty' do
+        board[5][5] = 2
+        expect(game.valid?(board, 5)).to eq(false)
+      end
+    end
+  end
+
+  describe '#next_available_row' do
+    context 'when there are 2 discs at the bottom' do
+      it 'returns the correct row' do
+        board[0][0] = 1
+        board[1][0] = 2
+
+        expect(game.next_available_row(board, 0)).to eq(2)
+      end
+    end
+  end
+end
